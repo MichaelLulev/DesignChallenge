5c7c08b4abd183ab8ffd95369f05107c7fa084f8
Michael Lulev <miki1990@gmail.com>
2024-01-22 18:18:24 +0200

    Begin implement Story

---
+++ src/cmps/HomePage/SuccessStories/Story.jsx
@@ -0,0 +1,11 @@
+
+
+
+export function Story({ paragraph, fullName, imgUrl }) {
+
+    return (
+        <article className="story">
+            <img src="/src/assets/img/quote.svg" alt="quote" />
+        </article>
+    )
+}
---
+++ src/cmps/HomePage/SuccessStories/StoryList.jsx
@@ -0,0 +1,18 @@
+
+
+
+export function StoryList() {
+    const stories = [
+        {
+            paragraph: '',
+            fullName: '',
+            imgUrl: '',
+        }
+    ]
+
+    return (
+        <section className="story-list">
+
+        </section>
+    )
+}
